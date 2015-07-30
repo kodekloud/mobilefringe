ae245aad7f32f18aee116aa05a8852fe2c39c2aa
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-07-30 14:36:13 -0400

    via kodekloud.io (55aebd796e6f647ab3050000)

--- assets/application.js
+++ assets/application.js
@@ -1,7 +1,7 @@
 /*Created 2015-07-21  by Rajbir Karan Singh*/
 
 $('.navlist a').click(function(e){  
-      $('html, body').stop().animate({scrollTop: $( $(this).attr('href') ).offset().top}, 800);
+    $('html, body').stop().animate({scrollTop: $( $(this).attr('href') ).offset().top}, 800);
     e.preventDefault();
 });
 
