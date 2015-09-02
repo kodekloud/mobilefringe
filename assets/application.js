053acfbe8834f1b84bcadedd86f21cb719cacf92
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-09-02 14:50:18 -0400

    via kodekloud.io (55aebd796e6f647ab3050000)

--- assets/application.js
+++ assets/application.js
@@ -123,3 +123,22 @@ function get_month (id){
     }
     return month;
 }
+
+function (){
+    $('#newsletter_form').submit(function(e){
+        e.preventDefault();
+        $.getJSON(
+            this.action + "?callback=?",
+            $(this).serialize(),
+            function (data) {
+                if (data.Status === 400) {
+                    alert("Please try again later.");
+                } else { // 200
+                    $("#success_subscribe").fadeIn();
+                }
+            }
+        );
+    });
+    
+}
+}
